453e87d0377358993262f68dc8a285d19c74dffd
MORGANJASON <420morganjason@gmail.com>
2023-03-22 11:43:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+console.log(cats);
+
+
+const cats = ["Milo", "Otis", "Garfield"]
+ 
+cats.push(["Ralph"]);
+
+console.log(cats);
+
+
+const cats = ["Milo", "Otis", "Garfield"]
+cats.unshift("Bob");
+console.log(cats);
+
+
+const cats = ["Milo", "Otis", "Garfield"]
+const oneDown = cats.pop();
+console.log(cats);
+
+const cats = ["Milo", "Otis", "Garfield"]
+cats.shift();
+console.log(cats);
+
+
+const cats = ["Milo", "Otis", "Garfield"]
+console.log(cats);
